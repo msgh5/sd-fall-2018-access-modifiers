9847e0985da2dd09af92fa714e33d81606d1c744
Guilherme Guizado <guilherme.guizado@mitt.ca>
2019-02-26 09:52:24 -0600

    First app commit

---
+++ Project1/Program.cs
@@ -0,0 +1,17 @@
+﻿using Project2;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var person = new Person();
+        }
+    }
+}
---
+++ Project2/Person.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project2
+{
+    public class Person
+    {
+        //Every code can access this property
+        public string Name { get; set; }
+
+        //Only Person or Derived classes can access this property
+        protected string LastName { get; set; }
+
+        //Only Person can access this property
+        private int Age { get; set; }
+
+        //Only codes inside this assembly (Project2) can access this property
+        internal string Address { get; set; }
+
+        public Person()
+        {
+            LastName = "Something";
+        }
+    }
+
+    public class Student : Person
+    {
+        public Student()
+        {
+            LastName = "Something";
+        }
+    }
+
+}
